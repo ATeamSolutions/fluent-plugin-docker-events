8cb33c69570599547696e70e4b8272df8dc0e767
Josef Karasek <jkarasek@redhat.com>
2017-08-24 14:49:05 +0200

    WIP handle errors from docker daemon and better format event structure

--- lib/fluent/plugin/in_docker_events.rb
+++ lib/fluent/plugin/in_docker_events.rb
@@ -1,66 +1,98 @@
 module Fluent
-    class DockerEventStreamInput < Input 
+    class DockerEventStreamInput < Input
         Fluent::Plugin.register_input('docker_events', self)
 
         config_param :tag, :string, :default => "docker"
         config_param :events, :string, :default => []
+        config_param :timeout, :integer, :default => 5
 
         # Define `router` method of v0.12 to support v0.10 or earlier
         unless method_defined?(:router)
-          define_method("router") { Fluent::Engine }
+            define_method("router") { Fluent::Engine }
         end
 
         def initialize
-          super
-          require 'docker'
+            super
+            require 'docker'
         end
 
         def configure(conf)
-          super
-          @events = @events.split(',').map {|event| event.strip }
+            super
+
+            if @timeout == nil || @timeout <= 0
+                raise Fluent::ConfigError "Param 'timeout' has unsupported value '#{@timeout}'"
+            end
+            if @events == nil || @events == []
+                raise Fluent::ConfigError "Param 'events' has unsupported value '#{@events}'"
+            end
+
+            @events = @events.split(',').map {|event| event.strip }
+            Docker.options[:read_timeout] = @timeout
+            @last_received = 0
+            @num_of_events = 0
+
+            log.debug "Configuration:\n", "Event list:", @events, "\n", "Timeout: ", @timeout
         end
 
         def start
-          super
-
-          @running = true
-          @thread = Thread.new(&method(:run))
+            super
+            @running = true
+            @thread = Thread.new(&method(:run))
         end
 
         def shutdown
-          @running = false
-          @thread.join
+            @running = false
+            @thread.join
+            log.info "Shutting down, number of events processed: #{@num_of_events}"
         end
 
-        def run     
+        def run
             while @running
-                Docker::Event.stream do |event|
-                    if @events.include?(event.action)
-                        emit(event)
+                log.debug "Entered 'while' loop. @running is '#{@running}', @last_received is '#{@last_received}'"
+                begin
+                    Docker::Event.stream(@last_received) do |event|
+                        log.debug "Received event #{event}. @running is '#{@running}', @last_received is '#{@last_received}'"
+                        if @events.include?(event.action)
+                            @last_received = event.timeNano
+                            @num_of_events += 1
+                            emit(event)
+                        end
                     end
+                rescue Docker::Error::TimeoutError => e
+                    log.error e.class, ": ", e
+                rescue Exception => e
+                    log.error e.class, ": ", e
                 end
             end
         end
 
         def emit(r)
-            record = {
-                "id" => r.id,
-                "action" => r.action,
-                "status" => r.status,
-                "type" => r.type,
-            }
-            _container = Thread.new{ results = Docker::Container.get(r.id) }.value
-            if _container
-                record["container"] = {
-                    "state" => _container.info["State"],
-                    "name" => _container.info["Name"].gsub(/^\//, ''),
+            begin
+                record = {
+                    "id" => r.id,
+                    "action" => r.action,
+                    "status" => r.status,
+                    "type" => r.type,
+                    "time" => r.time
                 }
-                _image = Thread.new{ results = Docker::Image.get(_container.info["Image"]) }.value
-                if _image
-                    record["container"]["image"] = _image.info["RepoTags"]
+                _container = Docker::Container.get(r.id)
+                if _container
+                    record["container"] = {
+                        "state" => _container.info["State"],
+                        "name" => _container.info["Name"].gsub(/^\//, ''),
+                    }
+                    _image = Docker::Image.get(_container.info["Image"])
+                    if _image
+                        record["container"]["image"] = _image.info["RepoTags"]
+                    end
                 end
+                event = {
+                    "event" => record
+                }
+                router.emit(@tag, Time.now.to_i, event)
+            rescue => e
+                log.error e.class, ": ", e
             end
-            router.emit(@tag, Time.now.to_i, record)
         end
     end
 end
